fb7bef7551fd774635bcddd9fbbc2f9e06d903b1
Andreyhuey <andreyhuey777@gmail.com>
2022-06-14 11:33:56 +0100

    Handle & Delete Child Components

--- src/Product.jsx
+++ src/Product.jsx
@@ -6,20 +6,59 @@ export default class Product extends Component {
   };
 
   render() {
+    console.log("Product");
+
     return (
       <div className="col-lg-6">
         <div className="card m-2">
           <div className="card-body">
-            <div className="text-muted"># {this.state.product.id}</div>
+            <div className="text-muted">
+              # {this.state.product.id}
+              <span
+                className="pull-right"
+                onClick={() => {
+                  this.props.onDelete(this.state.product);
+                }}
+              >
+                <i className="fa fa-times hand-icon"></i>
+              </span>
+            </div>
             <h5 className="pt-2 border-top">
-              {this.state.product.productName}
+              {this.state.product.productsName}
             </h5>
 
             <div>$ {this.state.product.price}</div>
           </div>
           {/* card body ends here */}
 
-          <div className="card-footer text-right">{this.props.children}</div>
+          <div className="card-footer">
+            <div className="float-start">
+              <span className="badge text-dark">
+                {this.state.product.quantity}
+              </span>
+              <div className="btn-group">
+                <button
+                  className="btn btn-outline-success"
+                  onClick={() => {
+                    this.props.onIncrement(this.state.product, 10);
+                    // 10 refers to the maximum number of products
+                  }}
+                >
+                  +
+                </button>
+                <button
+                  className="btn btn-outline-success"
+                  onClick={() => {
+                    this.props.onDrecement(this.state.product, 0);
+                    // 0 refers to the minimum quantiy of products
+                  }}
+                >
+                  -
+                </button>
+              </div>
+            </div>
+            <div className="float-end">{this.props.children}</div>
+          </div>
         </div>
       </div>
     );
--- src/ShoppingCart.jsx
+++ src/ShoppingCart.jsx
@@ -4,12 +4,12 @@ import Product from "./Product";
 export default class ShoppingCart extends Component {
   state = {
     products: [
-      { id: 1, productsName: "iPhone", price: 8900, quantity: 1 },
+      { id: 1, productsName: "iPhone 13", price: 8900, quantity: 0 },
       { id: 2, productsName: "Sony Camera", price: 4500, quantity: 0 },
       { id: 3, productsName: "Samsung QLED TV", price: 7745, quantity: 0 },
-      { id: 4, productsName: "iPad Pro", price: 12400, quantity: 1 },
-      { id: 5, productsName: "Xbox", price: 7780, quantity: 1 },
-      { id: 6, productsName: "Dell Monitor", price: 8900, quantity: 1 },
+      { id: 4, productsName: "iPad Pro", price: 12400, quantity: 0 },
+      { id: 5, productsName: "Xbox", price: 7780, quantity: 0 },
+      { id: 6, productsName: "Dell Monitor", price: 8900, quantity: 0 },
     ],
   };
 
@@ -22,7 +22,13 @@ export default class ShoppingCart extends Component {
           <div className="row">
             {this.state.products.map((prod) => {
               return (
-                <Product key={prod.id} product={prod}>
+                <Product
+                  key={prod.id}
+                  product={prod}
+                  onIncrement={this.handleIncrement}
+                  onDrecement={this.handleDecrement}
+                  onDelete={this.handleDelete}
+                >
                   <button className="btn btn-primary text-right">
                     Buy Now
                   </button>
@@ -34,4 +40,48 @@ export default class ShoppingCart extends Component {
       </>
     );
   }
+  // render here
+
+  // Executes when the user clicks on + button
+  handleIncrement = (product, maxValue) => {
+    // get index of the selected product
+    let allProducts = [...this.state.products];
+    let index = allProducts.indexOf(product);
+
+    if (allProducts[index].quantity < maxValue) {
+      allProducts[index].quantity++;
+
+      // update the state of current component(parent component)
+      this.setState({ products: allProducts });
+    }
+  };
+
+  // Executes when the user clicks on - button
+  handleDecrement = (product, minValue) => {
+    console.log("handle Decrement", product);
+    let allProducts = [...this.state.products];
+    let index = allProducts.indexOf(product);
+
+    if (allProducts[index].quantity > minValue) {
+      allProducts[index].quantity--;
+
+      // update the state of current component(parent component)
+      this.setState({ products: allProducts });
+    }
+  };
+
+  // executes when the user clicks on Delete(X button) in the product component.
+  handleDelete = (product) => {
+    let allProducts = [...this.state.products];
+    let index = allProducts.indexOf(product);
+
+    // confirmation for delete
+    if (window.confirm("Are you sure to delete?")) {
+      // delete product based on index
+      allProducts.splice(index, 1);
+
+      // update the state of current component(parent component)
+      this.setState({ products: allProducts });
+    }
+  };
 }
